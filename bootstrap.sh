0b6774847eff7a0aebdfa8479e2e8d14fa1b6bbf
Ayush Verma <yash109109@gmail.com>
2022-08-14 03:16:09 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -67,7 +67,7 @@ function build_inspector() {
   cd "$INSPECTOR_DIR"
 
   echo "Fetching Inspector dependencies..."
-  npm install
+  npm install --legecy-peer-deps @react-navigation/stack --force
 
   echo "Validating Inspector"
   "$INSPECTOR_DIR"/node_modules/.bin/eslint js/*
